280fd759e0d49009a3f552b718b21a42063dc6fe
Dave Welsh <dave@qccareerschool.com>
2024-06-28 09:34:24 -0400

    Updated VC image and text

--- src/components/virtualCommunitySection/index.tsx
+++ src/components/virtualCommunitySection/index.tsx
@@ -10,8 +10,8 @@ export const VirtualCommunitySection: FC = () => (
       <div className="row justify-content-center">
         <div className="col-12 col-lg-10">
           <h2 className="mb-3">Access an Exclusive Virtual Community Where Creativity Thrives</h2>
-          <p className="mb-0">We're here to inspire, inform, and innovate your educational journey. Discover the vibrant heart of our Virtual Community, where students and graduates connect to mentor, share insights, and grow together. Tap into free webinars for deep dives into design and industry secrets, and join lively chats for discussions that range from studies to career paths.</p>
-          <p className="text-black">Join us in shaping the future of design innovation.</p>
+          <p className="mb-0">Be a member of QC's supportive Virtual Community where students and graduates can connect with mentors, share insights and grow together. Tap into free webinars where top design professionals share industry secrets, and take part in lively discussions covering everything from your latest assignment to your next career move. We're here to inspire and enhance your educational journey.</p>
+          <p className="text-black">Join us in shaping the future of the design industry.</p>
         </div>
       </div>
       <div className="d-none d-sm-block"><Image src={DesktopImage} alt="" style={{ width: '100%', height: 'auto' }} /></div>
