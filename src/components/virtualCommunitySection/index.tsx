e1dd1c345969fe0205f9b4a384b931e9dd541750
Dave Welsh <dave@qccareerschool.com>
2024-06-28 10:00:23 -0400

    Fixed text

--- src/components/virtualCommunitySection/index.tsx
+++ src/components/virtualCommunitySection/index.tsx
@@ -11,7 +11,7 @@ export const VirtualCommunitySection: FC = () => (
         <div className="col-12 col-lg-10">
           <h2 className="mb-3">Access an Exclusive Virtual Community Where Creativity Thrives</h2>
           <p className="mb-0">Be a member of QC's supportive Virtual Community where students and graduates can connect with mentors, share insights and grow together. Tap into free webinars where top design professionals share industry secrets, and take part in lively discussions covering everything from your latest assignment to your next career move. We're here to inspire and enhance your educational journey.</p>
-          <p className="text-black">Join us in shaping the future of the design industry.</p>
+          <p className="fw-bold">Join us in shaping the future of the design industry.</p>
         </div>
       </div>
       <div className="d-none d-sm-block"><Image src={DesktopImage} alt="" style={{ width: '100%', height: 'auto' }} /></div>
