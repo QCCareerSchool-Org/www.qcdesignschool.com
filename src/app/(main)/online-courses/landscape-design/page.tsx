af231a3da9fd31a739f86ce20332991464ee2e24
Dave Welsh <dave@qccareerschool.com>
2024-07-17 17:24:16 -0400

    Added PO

--- src/app/(main)/online-courses/landscape-design/page.tsx
+++ src/app/(main)/online-courses/landscape-design/page.tsx
@@ -28,103 +28,101 @@ export const metadata: Metadata = {
 const testimonialIds = [ 'TD-0002', 'TD-0003', 'TD-0004', 'TD-0006', 'TD-0011', 'TD-0012' ];
 const courseCodes: CourseCode[] = [ 'ld' ];
 
-const LandscapeDesignPage: PageComponent = () => {
-  return (
-    <div className={styles.page}>
-      <section>
-        <div className="container">
-          <div className="row justify-content-center g-s">
-            <div className="col-12">
-              <Hero src={HeroImage} priority objectPosition="100% 0">
-                <CourseType className="mb-2 mb-xl-4">ILDP&trade; Certification</CourseType>
-                <h1 className="mb-2 mb-xl-4">Landscape Design Course</h1>
-                <p className="h5 mb-3 mb-xl-5">Become a Certified Landscape Designer</p>
-                <div className="d-flex justify-content-center justify-content-md-start">
-                  <HeroButtons courseCodes={courseCodes} />
-                </div>
-              </Hero>
-            </div>
-            <div className="col-12 col-lg-6">
-              <h2 className="mb-4">Why Choose<br />QC Design School</h2>
-              <p className="lead mb-4">QC's Landscape Design certification course provides students with the most comprehensive training available to succeed in the landscape design industry.</p>
-              <ul>
-                <li>No previous training required&mdash;our premium Landscape Design course is designed to take you from beginner to expert</li>
-                <li>Personalized feedback and advice on your assignments from our top landscape industry experts</li>
-                <li>Thriving virtual community to connect with other landscape designers, get feedback, and gain exclusive access to bonus material</li>
-                <li>Built-in business training and real-world templates to help you launch your career and grow your clientele as a certified Landscape Designer</li>
-                <li>Discounts and affiliations with top organizations including the American Society of Professional Organizers, Decorators & Designers Association of Canada, and more</li>
-              </ul>
-            </div>
-            <div className="col-12 col-sm-10 col-md-8 col-lg-6">
-              <Image src={WhyQCImage} alt="" className={`img-fluid ${styles.whyQCImage}`} />
-            </div>
-          </div>
-        </div>
-      </section>
-      <TestimonialWallSection testimonialIds={testimonialIds} courseCodes={courseCodes} className="bg-light" />
-      <section>
-        <div className="container">
-          <div className="row align-items-center justify-content-center g-s">
-            <div className="col-12 col-lg-6">
-              <h2>Here's What You'll Learn</h2>
-              <p>QC's comprehensive Landscape Design course will show you how to:</p>
-              <ul className="mb-0">
-                <li>Conduct an initial consultation and site visit that instills confidence in your clients</li>
-                <li>Confidently plan both the hardscape and softscape of a landscape site</li>
-                <li>Work with a variety of budget types, design requirements, and different client personalities</li>
-                <li>Understand plant classifications, physiology and crucial information about growing conditions</li>
-                <li>Develop high-quality, professional drawings that wow your clients</li>
-                <li>Build your brand and market your new landscape design business</li>
-                <li>Plan and develop a professional portfolio that helps you land new clients</li>
-              </ul>
-            </div>
-            <div className="col-12 col-sm-10 col-md-8 col-lg-6">
-              <Image src={WhatYoullLearnImage} alt="" className="img-fluid" />
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="text-white text-shadow">
-        <Image src={CertificationBackgroundImage} fill placeholder="blur" alt="" sizes="100vw" style={{ objectFit: 'cover' }} />
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-12 col-sm-10 col-lg-8 col-xxl-6 text-center">
-              <div>
-                <CertificationLogo height="200" className="mb-3" alt="International Landscape Design Professional (ILDP) certification logo" />
+const LandscapeDesignPage: PageComponent = () => (
+  <div className={styles.page}>
+    <section>
+      <div className="container">
+        <div className="row justify-content-center g-s">
+          <div className="col-12">
+            <Hero src={HeroImage} priority objectPosition="100% 0">
+              <CourseType className="mb-2 mb-xl-4">ILDP&trade; Certification</CourseType>
+              <h1 className="mb-2 mb-xl-4">Landscape Design Course</h1>
+              <p className="h5 mb-3 mb-xl-5">Become a Certified Landscape Designer</p>
+              <div className="d-flex justify-content-center justify-content-md-start">
+                <HeroButtons courseCodes={courseCodes} />
               </div>
-              <p className="mb-3 small gold">International Landscape Design Professional&trade; (ILDP&trade;)</p>
-              <h2 className="text-white mb-3">Your Landscape Design Certification</h2>
-              <p>Once you graduate from your Landscape Design course, you'll receive a certification and be able to use the designation of International Landscape Design Professional (ILDP).</p>
-              <p className="mb-0">This certification demonstrates that you have successfully completed Landscape Design training and that you possess all the skills and knowledge required to create functional and comfortable homes.</p>
-            </div>
+            </Hero>
+          </div>
+          <div className="col-12 col-lg-6">
+            <h2 className="mb-4">Why Choose<br />QC Design School</h2>
+            <p className="lead mb-4">QC's Landscape Design certification course provides students with the most comprehensive training available to succeed in the landscape design industry.</p>
+            <ul>
+              <li>No previous training required&mdash;our premium Landscape Design course is designed to take you from beginner to expert</li>
+              <li>Personalized feedback and advice on your assignments from our top landscape industry experts</li>
+              <li>Thriving virtual community to connect with other landscape designers, get feedback, and gain exclusive access to bonus material</li>
+              <li>Built-in business training and real-world templates to help you launch your career and grow your clientele as a certified Landscape Designer</li>
+              <li>Discounts and affiliations with top organizations including the American Society of Professional Organizers, Decorators & Designers Association of Canada, and more</li>
+            </ul>
+          </div>
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <Image src={WhyQCImage} alt="" className={`img-fluid ${styles.whyQCImage}`} />
           </div>
         </div>
-      </section>
-      <VirtualCommunitySection />
-      <TestimonialSection id="TD-0001" />
-      <section className="bg-light">
-        <div className="container">
-          <div className="row justify-content-center align-items-center g-lg-s">
-            <div className="col-12 col-lg-6 col-xl-7 col-xxl-6 mb-5 mb-lg-0">
-              <h2>Meet Your Landscape Design Instructor</h2>
-              <p className="mb-0">Chris Tuccio, professor and Program Coordinator of Horticulture & Landscape Design at Naugatuck Valley Community College, holds a Master's in Landscape Architecture from Harvard. He also serves on the CT Horticulture Society Board and manages Draftscapes.</p>
-            </div>
-            <div className="col-8 col-sm-6 col-lg-5 col-xl-4">
-              <Image src={ChrisTuccioCollageImage} alt="Chris Tuccio" className="img-fluid" />
-            </div>
+      </div>
+    </section>
+    <TestimonialWallSection testimonialIds={testimonialIds} courseCodes={courseCodes} className="bg-light" />
+    <section>
+      <div className="container">
+        <div className="row align-items-center justify-content-center g-s">
+          <div className="col-12 col-lg-6">
+            <h2>Here's What You'll Learn</h2>
+            <p>QC's comprehensive Landscape Design course will show you how to:</p>
+            <ul className="mb-0">
+              <li>Conduct an initial consultation and site visit that instills confidence in your clients</li>
+              <li>Confidently plan both the hardscape and softscape of a landscape site</li>
+              <li>Work with a variety of budget types, design requirements, and different client personalities</li>
+              <li>Understand plant classifications, physiology and crucial information about growing conditions</li>
+              <li>Develop high-quality, professional drawings that wow your clients</li>
+              <li>Build your brand and market your new landscape design business</li>
+              <li>Plan and develop a professional portfolio that helps you land new clients</li>
+            </ul>
+          </div>
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <Image src={WhatYoullLearnImage} alt="" className="img-fluid" />
           </div>
         </div>
-      </section>
-      <OutlineSection />
-      <CareerEssentialsKitSection />
-      <PaymentPlanSection courseCodes={courseCodes} />
-      <GetStartedSection
-        title="Ready to start your home design & decorating career?"
-        text="Become professionally certified with QC's online interior decorating training"
-        courseCodes={courseCodes}
-      />
-    </div>
-  );
-};
+      </div>
+    </section>
+    <section className="text-white text-shadow">
+      <Image src={CertificationBackgroundImage} fill placeholder="blur" alt="" sizes="100vw" style={{ objectFit: 'cover' }} />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-10 col-lg-8 col-xxl-6 text-center">
+            <div>
+              <CertificationLogo height="200" className="mb-3" alt="International Landscape Design Professional (ILDP) certification logo" />
+            </div>
+            <p className="mb-3 small gold">International Landscape Design Professional&trade; (ILDP&trade;)</p>
+            <h2 className="text-white mb-3">Your Landscape Design Certification</h2>
+            <p>Once you graduate from your Landscape Design course, you'll receive a certification and be able to use the designation of International Landscape Design Professional (ILDP).</p>
+            <p className="mb-0">This certification demonstrates that you have successfully completed Landscape Design training and that you possess all the skills and knowledge required to create functional and comfortable homes.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <VirtualCommunitySection />
+    <TestimonialSection id="TD-0001" />
+    <section className="bg-light">
+      <div className="container">
+        <div className="row justify-content-center align-items-center g-lg-s">
+          <div className="col-12 col-lg-6 col-xl-7 col-xxl-6 mb-5 mb-lg-0">
+            <h2>Meet Your Landscape Design Instructor</h2>
+            <p className="mb-0">Chris Tuccio, professor and Program Coordinator of Horticulture & Landscape Design at Naugatuck Valley Community College, holds a Master's in Landscape Architecture from Harvard. He also serves on the CT Horticulture Society Board and manages Draftscapes.</p>
+          </div>
+          <div className="col-8 col-sm-6 col-lg-5 col-xl-4">
+            <Image src={ChrisTuccioCollageImage} alt="Chris Tuccio" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <OutlineSection />
+    <CareerEssentialsKitSection />
+    <PaymentPlanSection courseCodes={courseCodes} />
+    <GetStartedSection
+      title="Ready to start your home design & decorating career?"
+      text="Become professionally certified with QC's online interior decorating training"
+      courseCodes={courseCodes}
+    />
+  </div>
+);
 
 export default LandscapeDesignPage;
--- src/app/(main)/online-courses/virtual-design/page.tsx
+++ src/app/(main)/online-courses/virtual-design/page.tsx
@@ -23,85 +23,67 @@ export const metadata: Metadata = {
 const testimonialIds = [ 'TD-0003', 'TD-0006', 'TD-0008', 'TD-0009', 'TD-0011', 'TD-0012' ];
 const courseCodes: CourseCode[] = [ 'vd' ];
 
-const VirtualDesignPage: PageComponent = () => {
-  return (
-    <div className={styles.page}>
-      <section>
-        <div className="container">
-          <div className="row justify-content-center g-s">
-            <div className="col-12">
-              <Hero src={HeroImage} priority objectPosition="100% 0">
-                <CourseType className="mb-2 mb-xl-4">Specialty Training</CourseType>
-                <h1 className="mb-2 mb-xl-4">Virtual Design</h1>
-                <p className="h5 mb-3 mb-xl-5">Expand Your Design Services Online</p>
-                <div className="d-flex justify-content-center justify-content-md-start">
-                  <HeroButtons courseCodes={courseCodes} />
-                </div>
-              </Hero>
-            </div>
-            <div className="col-12 col-lg-6">
-              <h2 className="mb-4">Why Choose<br />QC Design School</h2>
-              <p className="lead mb-4">QC's Virtual Design training provides students with the most comprehensive training available to help them expand their home design business.</p>
-              <ul>
-                <li>Revolutionize and expand your business by offering virtual services to your design clients</li>
-                <li>Personalized feedback and advice on your assignments from our top industry design experts</li>
-                <li>Thriving virtual community to connect with other decorators, get feedback and gain exclusive access to bonus materials</li>
-                <li>Built-in business training and real-world templates to help you launch your design career and grow your clientele as a certified interior decorator</li>
-                <li>Discounts and affiliations with top organizations including the American Society of Professional Organizers, Decorators & Designers Association of Canada, and more</li>
-              </ul>
-            </div>
-            <div className="col-12 col-sm-10 col-md-8 col-lg-6">
-              <Image src={WhyQCImage} alt="" className={`img-fluid ${styles.whyQCImage}`} />
-            </div>
-          </div>
-        </div>
-      </section>
-      <TestimonialWallSection testimonialIds={testimonialIds} courseCodes={courseCodes} className="bg-light" />
-      <section>
-        <div className="container">
-          <div className="row align-items-center justify-content-center g-s">
-            <div className="col-12 col-lg-6">
-              <h2>Here's What You'll Learn</h2>
-              <p>QC's Virtual Design training will allow you to expand your business beyond your local area, and gain a whole new sustainable client base. In one comprehensive training unit, this course will show you how to:</p>
-              <ul className="mb-0">
-                <li>Reach clients across the globe, all from the comfort of your home office</li>
-                <li>Transform your design process for the digital world and adapt your strategies and methods to thrive online</li>
-                <li>Cater to every client's needs and offer a spectrum of online services, from quick consultations to comprehensive redesigns</li>
-                <li>Discover the best online platforms to connect with your clients, provide unparalleled support, and build lasting relationships</li>
-              </ul>
-            </div>
-            <div className="col-12 col-sm-10 col-md-8 col-lg-6">
-              <Image src={WhatYoullLearnImage} alt="" className="img-fluid" />
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* <section className="text-white text-shadow">
-        <Image src={CertificationBackgroundImage} fill placeholder="blur" alt="" sizes="100vw" style={{ objectFit: 'cover' }} />
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-12 col-sm-10 col-lg-8 col-xxl-6 text-center">
-              <div>
-                <CertificationLogo height="200" className="mb-3" alt="International Floral Design Professional (IFDP) certification logo" />
+const VirtualDesignPage: PageComponent = () => (
+  <div className={styles.page}>
+    <section>
+      <div className="container">
+        <div className="row justify-content-center g-s">
+          <div className="col-12">
+            <Hero src={HeroImage} priority objectPosition="100% 0">
+              <CourseType className="mb-2 mb-xl-4">Specialty Training</CourseType>
+              <h1 className="mb-2 mb-xl-4">Virtual Design</h1>
+              <p className="h5 mb-3 mb-xl-5">Expand Your Design Services Online</p>
+              <div className="d-flex justify-content-center justify-content-md-start">
+                <HeroButtons courseCodes={courseCodes} />
               </div>
-              <p className="mb-3 small gold">International Floral Design Professional&trade; (IFDP&trade;)</p>
-              <h2 className="text-white mb-3">Your Virtual Design Certification</h2>
-              <p>Show your clients that you have the knowledge and skills to conduct design consultations remotely without compromising on the quality of your services.</p>
-              <p className="mb-0">This one-unit training course comes with a full certificate of completion. Your certificate demonstrates that you have been trained in virtual design. Display it proudly!</p>
-            </div>
+            </Hero>
+          </div>
+          <div className="col-12 col-lg-6">
+            <h2 className="mb-4">Why Choose<br />QC Design School</h2>
+            <p className="lead mb-4">QC's Virtual Design training provides students with the most comprehensive training available to help them expand their home design business.</p>
+            <ul>
+              <li>Revolutionize and expand your business by offering virtual services to your design clients</li>
+              <li>Personalized feedback and advice on your assignments from our top industry design experts</li>
+              <li>Thriving virtual community to connect with other decorators, get feedback and gain exclusive access to bonus materials</li>
+              <li>Built-in business training and real-world templates to help you launch your design career and grow your clientele as a certified interior decorator</li>
+              <li>Discounts and affiliations with top organizations including the American Society of Professional Organizers, Decorators & Designers Association of Canada, and more</li>
+            </ul>
+          </div>
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <Image src={WhyQCImage} alt="" className={`img-fluid ${styles.whyQCImage}`} />
           </div>
         </div>
-      </section> */}
-      <VirtualCommunitySection />
-      <TestimonialSection id="TD-0001" />
-      <PaymentPlanSection courseCodes={courseCodes} />
-      <GetStartedSection
-        title="Ready to expand your design services online?"
-        text="Become professionally certified with QC's online virtual design training"
-        courseCodes={courseCodes}
-      />
-    </div>
-  );
-};
+      </div>
+    </section>
+    <TestimonialWallSection testimonialIds={testimonialIds} courseCodes={courseCodes} className="bg-light" />
+    <section>
+      <div className="container">
+        <div className="row align-items-center justify-content-center g-s">
+          <div className="col-12 col-lg-6">
+            <h2>Here's What You'll Learn</h2>
+            <p>QC's Virtual Design training will allow you to expand your business beyond your local area, and gain a whole new sustainable client base. In one comprehensive training unit, this course will show you how to:</p>
+            <ul className="mb-0">
+              <li>Reach clients across the globe, all from the comfort of your home office</li>
+              <li>Transform your design process for the digital world and adapt your strategies and methods to thrive online</li>
+              <li>Cater to every client's needs and offer a spectrum of online services, from quick consultations to comprehensive redesigns</li>
+              <li>Discover the best online platforms to connect with your clients, provide unparalleled support, and build lasting relationships</li>
+            </ul>
+          </div>
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <Image src={WhatYoullLearnImage} alt="" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <TestimonialSection id="TD-0001" className="bg-light" />
+    <VirtualCommunitySection />
+    <PaymentPlanSection courseCodes={courseCodes} />
+    <GetStartedSection
+      title="Ready to expand your design services online?"
+      text="Become professionally certified with QC's online virtual design training"
+      courseCodes={courseCodes}
+    />
+  </div>
+);
 
 export default VirtualDesignPage;
