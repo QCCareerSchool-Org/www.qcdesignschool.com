cf46a516fd2e67b8ac08e7b8511164eb1b873b26
Owen Moogk <owenmoogk@gmail.com>
2024-08-16 12:33:42 -0400

    Add tutors page
    The blurbs for the tutors still needs updating, they're still the old event planning blurbs.

---
+++ src/app/(main)/tutors/page.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import { Blurb } from './blurb';
+import { CommunitySection } from './communitySection';
+import { tutors } from './tutorInformation';
+import type { PageComponent } from '@/app/serverComponent';
+import { TestimonialSection } from '@/components/testimonialSection';
+
+export const metadata: Metadata = {
+  title: 'Tutors - QC Event School',
+};
+
+const TutorsPage: PageComponent = () => (
+  <>
+    <section>
+      <div className="container">
+        <div className="row justify-content-center g-s">
+          <div className="col-12 col-xl-10 col-xxl-8 text-xl-center">
+            <h1 className="mb-3">QC Design School Tutors</h1>
+            <p className="lead mb-0">Once you enroll with QC Design School, you'll be matched with a personal tutor who will review your work and provide you with in-depth feedback and advice. Our accomplished tutors have years of experience in the event industry and are eager to share their professional insights with you.</p>
+          </div>
+          {tutors.map((t, i) => (
+            <div key={i} className="col-12">
+              <Blurb name={t.name} subHeading={t.subHeading} text={t.text} src={t.src} priority={i <= 1} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+    <CommunitySection />
+    <TestimonialSection id="TD-0013" />
+    <section className="bg-navy text-white">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-xl-10 col-xxl-8 text-center">
+            <h2 className="mb-3">Unlock Your Home Design Potential with Expert Insights!</h2>
+            <p className="lead mb-3">Dive into learning with individualized guidance from experts in event and wedding planning</p>
+            <div className="d-flex justify-content-center">
+              <Link className="btn btn-primary me-3" href="https://enroll.qcdesignschool.com">Enroll Now</Link>
+              <Link className="btn btn-outline-light" href="/online-courses">View Courses</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
+);
+
+export default TutorsPage;
---
+++ src/app/(main)/tutors/tutorInformation.tsx
@@ -0,0 +1,37 @@
+import AngieChapmanImage from '@/components/tutors/angieChapman.jpg';
+import ChristineKittelstadImage from '@/components/tutors/christina.jpg';
+import IvyEllerbyImage from '@/components/tutors/ivyEllerby.jpg';
+import JaneLockhartImage from '@/components/tutors/janeLockhart.jpg';
+import PatriziaBolandImage from '@/components/tutors/patrizia.jpg';
+
+export const tutors = [
+  {
+    name: 'Angie Chapman',
+    subHeading: 'Principal at Experience Events and Managing Director, Ingenuity Cleveland',
+    text: 'Alyssa has over a decade of event operations experience ranging from leading multinational business-to-business conferences, festivals and tradeshows, press conferences, large-scale fundraisers, complex social events and more.',
+    src: AngieChapmanImage,
+  },
+  {
+    name: 'Ivy Ellerby',
+    subHeading: 'Owner and Manager of Imagine Events, Wedding and Event Planning',
+    text: 'Angela became certified as an event and wedding planner in 2009, and graduated from QC Event School as an International Wedding Planning Professional. Now, Angela has over 10 successful years in business leading her own company and has worked for multiple non-profit organizations, bringing knowledge and experience to her role as an event planner.',
+    src: IvyEllerbyImage,
+  },
+  {
+    name: 'Christina Kittelstad',
+    text: 'An event & wedding planner with over 20 years in event management, Heather has produced a vast array of events in her diverse career, including hosting First Lady Michelle Obama at Spelman\'s Commencement.',
+    src: ChristineKittelstadImage,
+  },
+  {
+    name: 'Patrizia Boland',
+    subHeading: 'Owner of Grazie Gifts With over 9 years of experience',
+    text: 'Anita thrives on the challenge of creating proposals within budget, and coordinating entire themed events starting from the simplest spark of inspiration.',
+    src: PatriziaBolandImage,
+  },
+  {
+    name: 'Jane Lockhart',
+    subHeading: 'Owner and lead planner for Ashley Brooke Weddings',
+    text: 'With 10 years of experience in the wedding industry, she specializes in wedding planning, design and coordination and loves being able to create weddings that are unique to each of her couples.',
+    src: JaneLockhartImage,
+  },
+];
