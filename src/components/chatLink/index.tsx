5a0827a7eb3bea694d4977b5dde940ed484980e3
Owen Moogk <owenmoogk@gmail.com>
2024-08-20 10:29:56 -0400

    Modify ChatLink to accept the more versatile `className`

--- src/components/chatLink/index.tsx
+++ src/components/chatLink/index.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import type { CSSProperties, FC, MouseEventHandler } from 'react';
+import type { FC, MouseEventHandler } from 'react';
 
 interface Props {
-  color?: CSSProperties['color'];
+  className?: string;
 }
 
-export const ChatLink: FC<Props> = ({ color }) => {
+export const ChatLink: FC<Props> = ({ className }) => {
   const handleClick: MouseEventHandler<HTMLAnchorElement> = e => {
     e.preventDefault();
     window.LC_API?.open_chat_window?.();
   };
 
-  return <a onClick={handleClick} href="#" style={{ color: color }}>Chat with Support</a>;
+  return <a onClick={handleClick} href="#" className={className}>Chat with Support</a>;
 };
