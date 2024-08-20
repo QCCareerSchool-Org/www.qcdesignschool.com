4222a925fb27b2f62f3c49643d4290f4c9c65b0e
Owen Moogk <owenmoogk@gmail.com>
2024-08-20 10:04:23 -0400

    Fixed issue where the open_chat_window was undefined.
    This function is possibly undefined, handled it so an error doesn't come up.

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -9,7 +9,7 @@ interface ChatLinkProps {
 export const ChatLink: FC<ChatLinkProps> = ({ color }) => {
   const handleClick: MouseEventHandler<HTMLAnchorElement> = e => {
     e.preventDefault();
-    window.LC_API?.open_chat_window();
+    window.LC_API?.open_chat_window?.();
   };
 
   return <a onClick={handleClick} href="#" style={{ color: color }}>Chat with Support</a>;
