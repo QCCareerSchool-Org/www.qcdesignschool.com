4fac37f6a548486c3750d3ad10246556875ad3ef
Owen Moogk <owenmoogk@gmail.com>
2024-08-20 14:14:13 -0400

    Add `ProfileIcon` component for student-success page

---
+++ src/components/profileIcon/index.tsx
@@ -0,0 +1,24 @@
+import type { StaticImageData } from 'next/image';
+import Image from 'next/image';
+import type { FC } from 'react';
+import styles from './profileIcon.module.scss';
+
+type Props = {
+  image: StaticImageData;
+  name: string;
+  subtitle: string;
+};
+
+export const ProfileIcon: FC<Props> = ({ image, name, subtitle }) => {
+  return(
+    <div className={styles.profileIcon}>
+      <div className={styles.profileImageContainer}>
+        <Image src={image} alt="" className={styles.profileImage} />
+      </div>
+      <div className={styles.profileText}>
+        <span className="fw-bold text-dark">{name}</span>
+        <span>{subtitle}</span>
+      </div>
+    </div>
+  );
+};
