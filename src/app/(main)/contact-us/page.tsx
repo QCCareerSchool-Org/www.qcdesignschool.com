97eff66da327735ed7154badcd6ad7e41a152713
Dave Welsh <dave@qccareerschool.com>
2024-08-20 10:43:39 -0400

    Fixed email address

--- src/app/(main)/contact-us/page.tsx
+++ src/app/(main)/contact-us/page.tsx
@@ -6,6 +6,8 @@ import { GetStartedSection } from '@/components/getStartedSection';
 import EnvelopeIcon from '@/components/icons/envelope.svg';
 import MapIcon from '@/components/icons/map.svg';
 import PhoneIcon from '@/components/icons/phone.svg';
+import { TelephoneLink } from '@/components/telephoneLink';
+import { getData } from '@/lib/getData';
 
 export const metadata: Metadata = {
   title: 'Contact Us - QC Design School',
@@ -13,47 +15,50 @@ export const metadata: Metadata = {
 
 const iconHeight = 32;
 
-const ContactPage: PageComponent = () => (
-  <div>
-    <section>
-      <div className="container">
-        <div className="row justify-content-center mb-5">
-          <div className="col-12 col-md-10 col-lg-7 text-center">
-            <p className="text-primary fw-bold">We want to hear from you!</p>
-            <h1>Contact Us</h1>
-            <p className="mb-0">
-              Whether you're a prospective student curious about QC's courses or tuition, or a current student with questions about an assignment, we're here to assist you in any way possible.
-            </p>
+const ContactPage: PageComponent = () => {
+  const { countryCode } = getData();
+  return (
+    <div>
+      <section>
+        <div className="container">
+          <div className="row justify-content-center mb-5">
+            <div className="col-12 col-md-10 col-lg-7 text-center">
+              <p className="text-primary fw-bold">We want to hear from you!</p>
+              <h1>Contact Us</h1>
+              <p className="mb-0">
+                Whether you're a prospective student curious about QC's courses or tuition, or a current student with questions about an assignment, we're here to assist you in any way possible.
+              </p>
+            </div>
+          </div>
+          <div className="row justify-content-center g-4 g-lg-s">
+            <div className="col-12 col-md-8 col-lg-4 text-center">
+              <EnvelopeIcon height={iconHeight} className={`mb-3 ${styles.contactIcon}`} />
+              <h2 className="h4">Email</h2>
+              <p>Feel free to email us anytime, day or night, and we'll respond promptly.</p>
+              <a href="mailto:info@qcdesignschool.com" className={styles.blackLink}>info@qcdesignschool.com</a>
+            </div>
+            <div className="col-12 col-md-8 col-lg-4 text-center">
+              <PhoneIcon height={iconHeight} className={`mb-3 ${styles.contactIcon}`} />
+              <h2 className="h4">Phone</h2>
+              <p>Connect with a student advisor for guidance on enrolling with QC or assistance with your courses and assignments.</p>
+              <TelephoneLink className={styles.blackLink} countryCode={countryCode} />
+            </div>
+            <div className="col-12 col-md-8 col-lg-4 text-center">
+              <MapIcon height={iconHeight} className={`mb-3 ${styles.contactIcon}`} />
+              <h2 className="h4">Chat</h2>
+              <p>Have an urgent question? Our Student Support Specialists are standing by for a quick chat!</p>
+              <ChatLink color="black" />
+            </div>
           </div>
         </div>
-        <div className="row justify-content-center g-4 g-lg-s">
-          <div className="col-12 col-md-8 col-lg-4 text-center">
-            <EnvelopeIcon height={iconHeight} className={`mb-3 ${styles.contactIcon}`} />
-            <h2 className="h4">Email</h2>
-            <p>Feel free to email us anytime, day or night, and we'll respond promptly.</p>
-            <a href="mailto:info@qcdesign.com" className={styles.blackLink}>info@qcdesign.com</a>
-          </div>
-          <div className="col-12 col-md-8 col-lg-4 text-center">
-            <PhoneIcon height={iconHeight} className={`mb-3 ${styles.contactIcon}`} />
-            <h2 className="h4">Phone</h2>
-            <p>Connect with a student advisor for guidance on enrolling with QC or assistance with your courses and assignments.</p>
-            <a href="tel:1-833-600-3751" className={styles.blackLink}>1-833-600-3751</a>
-          </div>
-          <div className="col-12 col-md-8 col-lg-4 text-center">
-            <MapIcon height={iconHeight} className={`mb-3 ${styles.contactIcon}`} />
-            <h2 className="h4">Chat</h2>
-            <p>Have an urgent question? Our Student Support Specialists are standing by for a quick chat!</p>
-            <ChatLink color="black" />
-          </div>
-        </div>
-      </div>
-    </section>
-    <GetStartedSection
-      title="Ready to Enroll?"
-      text="You can get started with QC Design School whenever you like! When you enroll, you'll receive a login to the Online Student Center where you can access your course online."
-      buttonText="Enroll Online"
-    />
-  </div>
-);
+      </section>
+      <GetStartedSection
+        title="Ready to Enroll?"
+        text="You can get started with QC Design School whenever you like! When you enroll, you'll receive a login to the Online Student Center where you can access your course online."
+        buttonText="Enroll Online"
+      />
+    </div>
+  );
+};
 
 export default ContactPage;
