e1d98deafe2779ab17c9a2452837f7294494e96c
Owen Moogk <owenmoogk@gmail.com>
2024-09-06 20:24:52 +0200

    Complete `how-it-works` page

--- src/components/certifications/interior-decorating/index.tsx
+++ src/components/certifications/interior-decorating/index.tsx
@@ -21,3 +21,20 @@ export const CertificationSection: FC = () => (
     </div>
   </section>
 );
+
+export const CertificationSectionSmall: FC = () => (
+  <section className="text-white text-shadow">
+    <Image src={CertificationBackgroundImage} fill placeholder="blur" alt="" sizes="100vw" style={{ objectFit: 'cover' }} />
+    <div className="container">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-12 col-md-4">
+          <CertificationLogo height="200" className="mb-3" alt="International Design and Decorating Professional (IDDP) certification logo" />
+        </div>
+        <div className="col-12 col-md-8">
+          <h2 className="text-white mb-3">Become a Certified Design Professional with QC Design School!</h2>
+          <p>Once you have successfully completed all of your assignments and your tuition has been paid in full, you'll be all set to graduate from QC Design School! Your certificate of completion will be sent straight to you so you can begin your exciting career as a home designer.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+);
