cd0cacd5f210cf05c2c2bb0c9e60d98ffceadcc0
Owen Moogk <owenmoogk@gmail.com>
2024-09-08 11:19:42 +0200

    Add thank you course catalog pages

---
+++ src/app/(main)/(thank-you-course-catalog)/thank-you-home-staging-course-catalog/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next';
+import { LeadProcessingComponent } from '../leadProcessing';
+import ThankYouCourseCatalogComponent from '../thankYouPage';
+import type { PageComponent } from '@/app/serverComponent';
+
+export const metadata: Metadata = {
+  title: 'Your Course Catalog - QC Design School',
+};
+
+const ThankYouCourseCatalogPage: PageComponent = ({ searchParams }) => (
+  <>
+    <LeadProcessingComponent searchParams={searchParams} />
+    <ThankYouCourseCatalogComponent courseName="Home Staging" />
+  </>
+);
+
+export default ThankYouCourseCatalogPage;
