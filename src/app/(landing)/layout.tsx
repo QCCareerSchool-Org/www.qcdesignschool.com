d617ec135e9ea56ef507a2e4c4afa572d7488e98
Dave Welsh <dave@qccareerschool.com>
2024-09-17 13:13:58 -0400

    Fixed import

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 
 import { LayoutClient } from '../layoutClient';
-import { Footer } from './footer';
+import { Footer } from './_components/footer';
 import type { LayoutComponent } from '@/app/serverComponent';
 
 import '@/app/bootstrap.scss';
