3da0783aeed2b9db8c5c29514643cd4fdd73fc8b
Owen Moogk <owenmoogk@gmail.com>
2024-09-19 20:07:21 +0200

    Update review data and modify google component

--- src/components/googleReviewSection/index.tsx
+++ src/components/googleReviewSection/index.tsx
@@ -8,9 +8,10 @@ import GoogleLogo from './googleLogo.svg';
 import styles from './index.module.scss';
 import { InitialCircle } from './initialCircle';
 import type { ReviewData } from './reviewData';
-import { reviewData } from './reviewData';
+import { Priority, reviewData } from './reviewData';
 import { ImageCircle } from '../imageCircle';
 import { Star } from '../testimonial/star';
+import type { CourseCode } from '@/domain/courseCode';
 import { useScreenWidth } from '@/hooks/useScreenWidth';
 
 const responsive: ResponsiveType = {
@@ -20,8 +21,32 @@ const responsive: ResponsiveType = {
   },
 };
 
-export const GoogleReviewSection: FC = () => {
+// this will prioritize based on course code, and then priority
+const sortReviewData = (data: ReviewData[], course?: CourseCode): ReviewData[] => {
+  const addPriorityScore = (baseScore: number, priority: Priority | undefined): number => {
+    if (!priority) { priority = Priority.NORMAL; }
+    return baseScore + priority;
+  };
+  const highestPossiblePriority = Priority.HIGH;
+  // score both the courses, then return the difference
+  data.sort((a, b) => {
+    const scoreA = course && a.courseCodes?.includes(course)
+      ? addPriorityScore(highestPossiblePriority + 1, a.priority)
+      : addPriorityScore(0, a.priority);
+
+    const scoreB = course && b.courseCodes?.includes(course)
+      ? addPriorityScore(highestPossiblePriority + 1, b.priority)
+      : addPriorityScore(0, b.priority);
+
+    return scoreB - scoreA;
+  });
+  return data;
+};
+
+export const GoogleReviewSection: FC<{ courseCode?: CourseCode }> = ({ courseCode }) => {
+
   const screenWidth = useScreenWidth();
+  const sortedReviewData = sortReviewData(reviewData, courseCode);
 
   return (
     <section className="bg-light">
@@ -29,8 +54,8 @@ export const GoogleReviewSection: FC = () => {
         <div className="row justify-content-center">
           <div className="col-12 text-center">
             <GoogleLogo width="50" height="50" className="mb-3" />
-            <Carousel ssr responsive={responsive} infinite showDots={screenWidth < 992} arrows={screenWidth >= 992} autoPlay>
-              {reviewData.map((data, key) => <GoogleReview {...data} key={key} />)}
+            <Carousel ssr responsive={responsive} infinite showDots={screenWidth < 992} arrows={screenWidth >= 992}>
+              {sortedReviewData.map((data, key) => <GoogleReview {...data} key={key} />)}
             </Carousel>
           </div>
         </div>
@@ -39,14 +64,14 @@ export const GoogleReviewSection: FC = () => {
   );
 };
 
-const GoogleReview: FC<ReviewData> = ({ name, initial, imageSrc, backgroundColor, reviewText, size, rating }) => (
+const GoogleReview: FC<ReviewData> = ({ name, imageSrc, reviewText, size, backgroundColor }) => (
   <div className={styles.wrapper}>
-    <div className="mb-3">{Array(5).fill(null).map((_, i) => <Star key={i} filled={rating > i} />)}</div>
+    <div className="mb-3">{Array(5).fill(null).map((_, i) => <Star key={i} filled={true} />)}</div>
     <p className="fw-bold mb-4" style={size ? { fontSize: `${size}rem` } : undefined}>&quot;{reviewText}&quot;</p>
     <div className="d-flex justify-content-center mb-2">
       {imageSrc
         ? <ImageCircle src={imageSrc} alt={name} />
-        : <InitialCircle initial={initial} backgroundColor={backgroundColor} />
+        : backgroundColor && <InitialCircle initial={name.substring(0, 1)} backgroundColor={backgroundColor} />
       }
     </div>
     <p className="fw-bold">{name}</p>
