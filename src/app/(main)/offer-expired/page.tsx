b2300d27f42e93081ff2d014acb9afc158ca1f82
Owen Moogk <owenmoogk@gmail.com>
2024-09-28 10:46:15 +0200

    Add offer expired page

---
+++ src/app/(main)/offer-expired/page.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+
+import Background from './background.jpeg';
+import type { PageComponent } from '@/app/serverComponent';
+import { BackgroundImage } from '@/components/backgroundImage';
+import { ChatLink } from '@/components/chatLink';
+import { TelephoneLink } from '@/components/telephoneLink';
+import { getData } from '@/lib/getData';
+
+export const metadata: Metadata = {
+  title: 'QC Design School',
+};
+
+const { countryCode } = getData();
+
+const OfferExpiredPage: PageComponent = () => (
+  <section>
+    <BackgroundImage priority src={Background} />
+    <div className="container py-5 text-white text-center">
+      <div className="row justify-content-center ">
+        <div className="col-12 col-md-10 col-lg-8 col-xxl-6">
+          <h1 className="mb-4">Oops! Your Offer Has Expired!</h1>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-10 col-lg-8 col-xxl-6">
+          <p className="lead mb-4">But there may be something else we can do for you. Talk to one of our Student Advisors to learn about all of QC's special offers and discounts.</p>
+          <TelephoneLink countryCode={countryCode} className="btn btn-outline-light m-2" linkText="Call Us Now" />
+          <ChatLink className="btn btn-primary m-2" />
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default OfferExpiredPage;
