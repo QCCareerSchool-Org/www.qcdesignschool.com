b33a3aafe88052fe103225c54507f39e10247da9
Dave Welsh <dave@qccareerschool.com>
2024-10-02 15:54:10 -0400

    Updated about page

---
+++ src/app/(main)/about/meetOurTeamSection.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import type { FC } from 'react';
+
+import styles from './meetOurTeamSection.module.scss';
+import JoannePendonImage from '@/components/employees/joanne-pendon.jpg';
+import LucieDemersImage from '@/components/employees/lucie-demers.jpg';
+import MalcolmAsselinImage from '@/components/employees/malcolm-asselin.jpg';
+import QIcon from '@/components/icons/q-logo.svg';
+import { ImageCircle } from '@/components/imageCircle';
+
+type Props = {
+  className?: string;
+};
+
+const imageSize = 200;
+
+export const MeetOurTeamSection: FC<Props> = ({ className }) => (
+  <section className={className}>
+    <div className="container">
+      <div className="row justify-content-center mb-5 text-lg-center">
+        <div className="col-12 col-lg-10 col-xl-8">
+          <h2 className="mb-4">Meet Our Team</h2>
+          <p className="lead mb-0">At QC, we believe that the heart of a great learning experience lies in the people who support you every step of the way. Our dedicated team of experts  are here to ensure that your journey with us is rewarding and personalized. From the first lesson to graduation day, we're committed to helping you succeed and reach your full potential.</p>
+        </div>
+      </div>
+      <div className="row justify-content-center g-5 mb-5">
+        <div className="col-12 col-md-8 col-lg-4">
+          <div className="mb-4">
+            <ImageCircle size={imageSize} src={JoannePendonImage} alt="Joanne Pendon" />
+          </div>
+          <h3 className={styles.heading}>Joanne, CEO</h3>
+          <p className="mb-0 small">At QC, my team is committed to delivering innovation and quality at every step of your learning journey. Our goal is to blend innovation with connection, so you always feel supported throughout your journey. Most importantly, we're dedicated to providing top-tier education at an affordable price, empowering you to confidently launch a successful business or career.</p>
+        </div>
+        <div className="col-12 col-md-8 col-lg-4">
+          <div className="mb-4">
+            <ImageCircle size={imageSize} src={MalcolmAsselinImage} alt="Joanne Pendon" />
+          </div>
+          <h3 className={styles.heading}>Malcolm, Student Success Manager</h3>
+          <p className="mb-0 small">My top priority at QC is to enrich your learning experience by fostering a strong, supportive community where you can connect with fellow students and graduates, and learn from industry experts. I want to make sure you have every resource you need right at your fingertips.</p>
+        </div>
+        <div className="col-12 col-md-8 col-lg-4">
+          <div className="mb-4">
+            <ImageCircle size={imageSize} src={LucieDemersImage} alt="Joanne Pendon" />
+          </div>
+          <h3 className={styles.heading}>Lucie, Student Coordinator</h3>
+          <p className="mb-0 small">I'm here to support you from the moment you begin your journey with QC through to graduation. I oversee the enrollment process and personally welcome you to the school to ensure you get off to a great start. If you have any questions along the way, I'm just a phone call away!</p>
+        </div>
+      </div>
+
+      <div className="text-center">
+        <Link href="meet-the-team" className="btn btn-primary">Meet the entire QC family!<QIcon height="16" className="ms-2" style={{ position: 'relative', top: -1 }} /></Link>
+      </div>
+    </div>
+  </section>
+);
--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -2,23 +2,20 @@ import type { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { CoreValuesSection } from './coreValuesSection';
+import { MeetOurTeamSection } from './meetOurTeamSection';
 import styles from './page.module.scss';
-import WhyQCImage from './why-qc.jpg';
 import type { PageComponent } from '@/app/serverComponent';
 import { DesignNetworksSection } from '@/components/designNetworksSection';
-import AccessibilityIcon from '@/components/icons/accessibility.svg';
 import BBBIcon from '@/components/icons/bbb.svg';
-import HeadphoneIcon from '@/components/icons/headphones.svg';
-import LightbulbIcon from '@/components/icons/lightbulb.svg';
 import ShieldIcon from '@/components/icons/shield.svg';
-import QualityIcon from '@/components/icons/star-award.svg';
 import HowYoullLearnImage from '@/images/how-youll-learn.jpg';
 
 export const metadata: Metadata = {
-  title: 'About QC - QC Design School',
+  title: 'About the School - QC Design School',
 };
 
-const iconSize = 40;
+const iconSize = 48;
 
 const AboutPage: PageComponent = () => (
   <div className={styles.aboutPage}>
@@ -38,67 +35,15 @@ const AboutPage: PageComponent = () => (
         </div>
       </div>
     </section>
-    <section>
-      <div className="container">
-        <div className="row align-items-center justify-content-center g-4 g-xl-5 g-xxl-s">
-          <div className="col-12 col-md-10 col-lg-8 col-xl-6 col-xxl-7 text-center text-lg-start">
-            <h2 className="mb-5 mb-lg-4 mb-xxl-5">Core Values</h2>
-            <div className="row g-5 g-lg-4 g-xxl-5">
-              <div className="col-12 col-lg-6">
-                <QualityIcon height="32" className={`${styles.icon} d-lg-none d-xxl-block mb-3`} />
-                <h3 className="h6 mb-3">Quality</h3>
-                <p className="mb-0">You'll see QC's commitment to quality reflected in every aspect of your design courses.</p>
-                <ul className="text-start">
-                  <li>Expert-reviewed course lessons</li>
-                  <li>Custom-designed Online Student Center</li>
-                  <li>Industry-focused webinars</li>
-                </ul>
-              </div>
-              <div className="col-12 col-lg-6">
-                <HeadphoneIcon height="32" className={`${styles.icon} d-lg-none d-xxl-block mb-3`} />
-                <h3 className="h6 mb-3">Enjoy Self-Paced Learning</h3>
-                <p className="mb-0">QC provides a fully online education so that you can learn at your own pace with support from student advisors, tutors and a network of peers in our private virtual community!</p>
-                <ul className="text-start">
-                  <li>Assignment support 7 days a week</li>
-                  <li>Feedback and guidance from industry experts</li>
-                  <li>A vibrant virtual community of design students and professionals</li>
-                </ul>
-              </div>
-              <div className="col-12 col-lg-6">
-                <LightbulbIcon height="32" className={`${styles.icon} d-lg-none d-xxl-block mb-3`} />
-                <h3 className="h6 mb-3"><span className="d-lg-none d-xxl-inline">Earn </span>Industry-Recognized Certifications</h3>
-                <p className="mb-0">Graduate with professional home design certifications that will open up a world of career &amp; business opportunities to jumpstart your career in the booming design industry.</p>
-                <ul className="text-start">
-                  <li>Interactive digital lessons</li>
-                  <li>Engaging instructional videos</li>
-                  <li>Real-world case studies</li>
-                </ul>
-              </div>
-              <div className="col-12 col-lg-6">
-                <AccessibilityIcon height="32" className={`${styles.icon} d-lg-none d-xxl-block mb-3`} />
-                <h3 className="h6 mb-3">Prepare for a Successful Career</h3>
-                <p className="mb-0">Each course includes built-in business training and expert-led webinars to help you launch your career and grow your clientele as a newly certified designer.</p>
-                <ul className="text-start">
-                  <li>Affordable tuition and customized payment plans</li>
-                  <li>Flexible learning schedule</li>
-                  <li>Convenient, easy-to-access online courses</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="d-none d-lg-block col-lg-4 col-xl-6 col-xxl-5">
-            <Image src={WhyQCImage} alt="" className="img-fluid" />
-          </div>
-        </div>
-      </div>
-    </section>
+    <MeetOurTeamSection className="bg-light" />
+    <CoreValuesSection />
     <DesignNetworksSection showLink />
     <section className="bg-light">
       <div className="container">
         <div className="row g-5 g-xl-s">
           <div className="col-12 col-lg-6">
             <BBBIcon height={iconSize} className={styles.goldIcon} />
-            <h2 className="h4 mb-3">20 Years of Accreditation with the BBB</h2>
+            <h2 className="h4 mb-3">20 Years of Accreditation<span className="d-lg-none d-xxl-inline"> with the BBB</span></h2>
             <p>QC is proud to have held the highest possible consumer satisfaction rating with the Better Business Bureau for over 20 years. Our A+ rating reflects our commitment to quality and student satisfaction.</p>
             <p className="mb-0">QC Design School has had thousands of satisfied students take our courses, with many going to launch lucrative design businesses and build successful careers.</p>
           </div>
@@ -106,7 +51,7 @@ const AboutPage: PageComponent = () => (
             <ShieldIcon height={iconSize} className={styles.goldIcon} />
             <h2 className="h4 mb-3">21-Day Money-Back Guarantee</h2>
             <p>Once you enroll with QC Design School, you'll be protected by a money-back guarantee!</p>
-            <p className="mb-0">Once your enrollment has been processed, you have 21 days to review the course materials and decide whether you want to continue. If you decide this course isn't the right course for you, simply contact the school to request a full refund.Additional information on QC's money-back guarantee is available in your enrollment</p>
+            <p className="mb-0">Once your enrollment has been processed, you have 21 days to review the course materials and decide whether you want to continue. If you decide this course isn't the right course for you, simply contact the school to request a full refund. Additional information on QC's money-back guarantee is available in your enrollment agreement.</p>
           </div>
         </div>
       </div>
