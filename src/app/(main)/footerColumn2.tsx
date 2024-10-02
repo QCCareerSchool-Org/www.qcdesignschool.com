28edbddec514424f8c1585ac80a39bd8b587ab3a
Dave Welsh <dave@qccareerschool.com>
2024-10-02 18:56:21 -0400

    Fixed footer link

--- src/app/(main)/footerColumn2.tsx
+++ src/app/(main)/footerColumn2.tsx
@@ -7,7 +7,7 @@ export const FooterColumn2: FC = () => (
     <li><Link href="/student-success">Student Success</Link></li>
     <li><Link href="/instructors">Instructors</Link></li>
     <li><Link href="/about">About the School</Link></li>
-    <li><Link href="/design-resources">Design Resources</Link></li>
+    <li><Link href="/design-associations">Design Associations</Link></li>
     <li><a href="/blog">Blog</a></li>
     <li><a href="https://studentcenter.qccareerschool.com/students" target="_blank" rel="noreferrer">Student Login</a></li>
   </>
