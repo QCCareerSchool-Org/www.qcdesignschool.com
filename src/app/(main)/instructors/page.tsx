491f78e685fb0cd012684d5dd765de6a83e21c96
Owen Moogk <owenmoogk@gmail.com>
2024-10-08 21:58:04 +0200

    Update instructor information (except for bad image)

--- src/app/(main)/instructors/page.tsx
+++ src/app/(main)/instructors/page.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next';
 import Link from 'next/link';
 
 import { Blurb } from './blurb';
-import { tutors } from './tutorInformation';
+import { experts, tutors } from './tutorInformation';
 import type { PageComponent } from '@/app/serverComponent';
-import { TestimonialSection } from '@/components/testimonialSection';
+import { GoogleReviewSection } from '@/components/googleReviewSection';
 import { VirtualCommunitySection } from '@/components/virtualCommunitySection';
 
 export const metadata: Metadata = {
@@ -18,19 +18,19 @@ const TutorsPage: PageComponent = () => (
         <div className="row justify-content-center g-s">
           <div className="col-12 col-xl-10 col-xxl-8 text-xl-center">
             <h1 className="mb-3">QC Design School Instructors</h1>
-            <p className="lead mb-0">Once you enroll with QC Design School, you'll be matched with a personal instructor who will review your work and provide you with in-depth feedback and advice. You'll also have the opportunity to learn from top industry experts who have worked with QC to develop your cutting-edge course materials. Our accomplished instructors have years of experience in the industry and are eager to share their professional insights with you.</p>
+            <p className="lead mb-0">QC Design School is proud to work with many successful design industry professionals who will be your instructors as you work toward your certification. Your instructors include personal tutors as well as content experts. Once you enroll in your course, you'll be matched with a personal tutor who will review your work and provide you with in-depth feedback and advice. You'll also have the opportunity to learn from top industry experts who have worked with QC to develop your cutting-edge course materials. Our accomplished instructors have years of experience in the industry and are eager to share their professional insights with you.</p>
           </div>
-          <div className="row justify-content-center g-5">
-            <h2 className="text-center my-0">Instructors</h2>
-            {tutors.map((t, i) => t.type === 'instructor' && (
+          <div className="row justify-content-center g-5 mt-s">
+            <h2 className="text-center my-0">QC Experts</h2>
+            {experts.map((t, i) => (
               <div key={i}>
                 <Blurb name={t.name} description={t.description} src={t.src} priority={i <= 1} />
               </div>
             ))}
           </div>
-          <div className="row justify-content-center g-5 mt-s">
-            <h2 className="text-center my-0">QC Experts</h2>
-            {tutors.map((t, i) => t.type === 'expert' && (
+          <div className="row justify-content-center g-5">
+            <h2 className="text-center my-0">Tutors</h2>
+            {tutors.map((t, i) => (
               <div key={i}>
                 <Blurb name={t.name} description={t.description} src={t.src} priority={i <= 1} />
               </div>
@@ -40,7 +40,7 @@ const TutorsPage: PageComponent = () => (
       </div>
     </section>
     <VirtualCommunitySection />
-    <TestimonialSection id="TD-0013" />
+    <GoogleReviewSection />
     <section className="bg-navy text-white">
       <div className="container">
         <div className="row justify-content-center">
--- src/app/(main)/instructors/tutorInformation.tsx
+++ src/app/(main)/instructors/tutorInformation.tsx
@@ -1,48 +1,82 @@
 import type { StaticImageData } from 'next/image';
 
 import AngieChapmanImage from '@/components/tutors/angieChapman.jpg';
+import ChrisTuccioImage from '@/components/tutors/chris.jpg';
 import ChristineKittelstadImage from '@/components/tutors/christina.jpg';
+import DarcieGarciaImage from '@/components/tutors/darcie.jpg';
 import IvyEllerbyImage from '@/components/tutors/ivyEllerby.jpg';
 import JaneLockhartImage from '@/components/tutors/janeLockhart.jpg';
+import LianneCouvisImage from '@/components/tutors/lianne.jpg';
+import MwaiYeboahImage from '@/components/tutors/mwai.jpg';
 import PatriziaBolandImage from '@/components/tutors/patrizia.jpg';
+import ReneeTucciImage from '@/components/tutors/reneeTucci.jpg';
+import TammyHartImage from '@/components/tutors/tammyHart.jpg';
 
-type InstructorType = 'instructor' | 'expert';
-type TutorInformation = {
+type InstructorInformation = {
   name: string;
   description: string;
   src: StaticImageData;
-  type: InstructorType;
 };
 
-export const tutors: TutorInformation[] = [
+export const experts: InstructorInformation[] = [
+  {
+    name: 'Renee Tucci',
+    description: 'With over 20 years of floral design experience, Renee Tucci, recognized by the American Institute of Floral Designers and Professional Floral Communicators International, is a dedicated instructor in the QC course, guiding students to master the art of floral design.',
+    src: ReneeTucciImage,
+  },
+  {
+    name: 'Mwai Yeboah',
+    description: 'Mwai Yeboah, founder of Love From Mwai and multi-award winning planner, specializes in destination wedding planning. As the lead planner and designer, she transforms weddings and events into immersive experiences for luxury clients worldwide.',
+    src: MwaiYeboahImage,
+  },
   {
     name: 'Angie Chapman',
-    description: 'Award-winning interior design consultant and QC tutor Angie Chapman has always loved all aspects of interior design, decorating and architecture. After receiving diplomas in both Interior Design and Decorating and Home Staging from the Sheffield School of Interior Design, Angie started her own business, Interiors Just for You, in Peterborough, Ontario in 1998.',
+    description: 'Award-winning interior design consultant and founder of Angie Chapman Interiors, Angie is a driving force in the design world. With over 10 years of experience in the industry, she has honed her skills and expertise to become an sought-after designer.',
     src: AngieChapmanImage,
-    type: 'instructor',
   },
   {
     name: 'Ivy Ellerby',
-    description: 'For Accredited Staging Professional and Advanced Feng Shui Design Professional Ivy Ellerby, the inspiration to pursue a design career started at a very young age. Ivy\'s background in clinical and experimental psychology allows her to use feng shui to increase harmony and balance in her clients\' homes.',
+    description: 'Accredited Staging Professional, Advanced Feng Shui Design Professional, and consultant at Eastern Design and Home Staging, Ivy blends her background in clinical and experimental psychology to enhance harmony and balance in homes.',
     src: IvyEllerbyImage,
-    type: 'instructor',
-  },
-  {
-    name: 'Christina Kittelstad',
-    description: 'Christina Kittelstad is an accomplished color expert and owner of Spiral Design Color Consulting in Lakewood, Colorado. Through her lifelong passion for all things color, Christina brings a high level of creativity, professionalism, and talent to every project. She partners with residential and commercial clients and is best known for creating beautiful, functional spaces on any budget. She provides clients with a strong sense of style and personality that is uniquely their own.',
-    src: ChristineKittelstadImage,
-    type: 'instructor',
-  },
-  {
-    name: 'Patrizia Boland',
-    description: 'Designing interiors and my beautiful family are the two greatest passions in Patrizia\'s life. Patrizia graduated from QC Design School as a certified International Design and Decorating Professional and went on to launch Boland Designs. Her approach to design is to take a holistic view of the client\'s entire space and how they wish to live in it. Patrizia enjoys helping transform her client\'s space into a beautiful environment that reflects their personality and functionality.',
-    src: PatriziaBolandImage,
-    type: 'instructor',
   },
   {
     name: 'Jane Lockhart',
-    description: 'Many will recognize Jane from her television show, Color Confidential, which airs on both HGTV and W Network. Jane uses both her design expertise and kind demeanor to help people overcome their fear of color. As the host of QC Design School\'s Color Consultant course videos, Jane teaches students how to use the color wheel in order to help clients choose the best colors for their home. She demonstrates how to conduct a professional color consultation, as well as how to build a strong relationship between art and design in the home.',
+    description: 'Many recognize Jane from her show, Color Confidential, on HGTV and W Network. She is also the best-selling author of two books and frequently lends her design expertise to Canadian television shows including The Marilyn Denis Show and Cityline.',
     src: JaneLockhartImage,
-    type: 'expert',
   },
+  {
+    name: 'Chris Tuccio',
+    description: 'Chris Tuccio, professor and Program Coordinator of Horticulture & Landscape Design at Naugatuck Valley Community College, holds a Master\'s in Landscape Architecture from Harvard. He also serves on the CT Horticulture Society Board and manages Draftscapes',
+    src: ChrisTuccioImage,
+  },
+  {
+    name: 'Tammy Hart',
+    description: 'Tammy is the owner of Designer Chick Co. She also served as a director on the National Board for DDA. Tammy has presented at IIDEXCanada and The Small Business Forum, been featured in East of the City Magazine, and appeared on the Rogers Daytime Durham talk show.',
+    src: TammyHartImage,
+  },
+];
+
+export const tutors: InstructorInformation[] = [
+  {
+    name: 'Lianne Couvis',
+    description: 'Lianne, owner of ChiChi Fringo Designs in Toronto, is a QC Design School graduate with certifications in decorating, staging, color consulting, and organizing. Originally from the Bahamas, she blends vibrant colors, textures, and patterns in her designs. With 20 years in film and TV, Lianne continues to find daily inspiration in design.',
+    src: LianneCouvisImage,
+  },
+  {
+    name: 'Christina Kittelstad',
+    description: 'Christina, owner of Spiral Design Color Consulting in Lakewood, Colorado, excels in creating beautiful, functional spaces on any budget. Known for her creativity and professionalism, Christina collaborates with residential and commercial clients. Her work has been featured on HGTV’s House Hunters, Houzz, The Denver Post, and more.',
+    src: ChristineKittelstadImage,
+  },
+
+  {
+    name: 'Patrizia Boland',
+    description: 'Graduating from QC Design School as a certified International Design and Decorating Professional, Patricia launched Boland Designs. Patrizia’s holistic approach transforms clients’ spaces into beautiful, functional environments reflective of their personalities. As a QC tutor, she helps students achieve their highest potential in the design & decorating industry.',
+    src: PatriziaBolandImage,
+  },
+  {
+    name: 'Darcie Garcia',
+    description: 'Darcie Garcia, owner of The Florapest Raleigh, has 32 years of floral industry experience and is an accredited educator by the American Institute of Floral Designers. She has managed large flower shops and traveled nationally to teach. Darcie\'s nurturing approach encourages creativity and confidence, inspiring aspiring designers to excel in the floral industry.',
+    src: DarcieGarciaImage,
+  },
+
 ];
