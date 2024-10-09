6d9f865ec55bc77022243ac9adba9d75ca749a1b
Dave Welsh <dave@qccareerschool.com>
2024-10-09 14:39:53 -0400

    Added subtitle

--- src/components/testimonialVideoSection/index.tsx
+++ src/components/testimonialVideoSection/index.tsx
@@ -5,8 +5,10 @@ import { Lightbox } from 'yet-another-react-lightbox';
 import Video from 'yet-another-react-lightbox/plugins/video';
 
 import 'yet-another-react-lightbox/styles.css';
+import styles from './index.module.scss';
 import { VideoCard } from './videoCard';
 import { videos, videoSlides } from './videos';
+import { CountUp } from '../paymentPlanSection/countUp';
 
 export const TestimonialVideoSection: FC = () => {
   const [ slideIndex, setSlideIndex ] = useState<number>(0);
@@ -17,9 +19,10 @@ export const TestimonialVideoSection: FC = () => {
       <section className="bg-light">
         <div className="container">
           <div className="row justify-content-center text-center mb-5">
-            <div className="col-12 col-md-10 col-lg-8">
-              <h2>Join 45,000+ Student & Graduates Shaping the Future of Design</h2>
-              <p>With over 45,000 students & graduates and 40 years in distance education, QC Design School empowers your passion for design and transforms it into a successful career. Learn from the best and join a community that's redefining the industry.</p>
+            <div className="col-12 col-md-10 col-xl-9 col-xxl-8">
+              <div className={styles.subtitle}>40 Years of Excellence in Distance Education</div>
+              <h2 className="mb-4">Join <CountUp value={45} />,000+ Student &amp; Graduates Shaping the Future of Design</h2>
+              <p className="lead mb-0">With over 45,000 students &amp; graduates and 40 years in distance education, QC Design School empowers your passion for design and transforms it into a successful career. Learn from the best and join a community that's redefining the industry.</p>
             </div>
           </div>
           <div className="row justify-content-center g-5">
