41a539224a44dcc5feeda04a0e64bdf1d55bf4f2
Dave Welsh <dave@qccareerschool.com>
2024-10-09 14:16:16 -0400

    Updated design associations page

--- src/app/(main)/design-associations/page.tsx
+++ src/app/(main)/design-associations/page.tsx
@@ -5,6 +5,7 @@ import { DesignAssociationSection } from './designAssociationSection';
 import styles from './page.module.scss';
 import type { PageComponent } from '@/app/serverComponent';
 import { BackgroundImage } from '@/components/backgroundImage';
+import { GetStartedSection } from '@/components/getStartedSection';
 
 export const metadata: Metadata = {
   title: 'Preferred Partners - QC Design School',
@@ -14,18 +15,22 @@ const HomePage: PageComponent = () => (
   <>
     <section className="bg-light">
       <div className="d-none d-md-block">
-        <BackgroundImage priority src={Background} />
+        <BackgroundImage priority src={Background} objectPosition="80% 50%" />
       </div>
       <div className={`container ${styles.container}`}>
         <div className="row justify-content-end">
-          <div className="col-md-6 col-lg-5">
-            <h1 className="mb-4">Professional Associations &amp; Preferred Partners</h1>
+          <div className="col-md-6 col-xl-5">
+            <h1 className="h2 mb-4">Professional Associations &amp; Preferred Partners</h1>
             <p className="lead mb-0">Design associations offer opportunities to network and enhance your professional skills. Students and graduates of QC Design School have access to the following associations:</p>
           </div>
         </div>
       </div>
     </section>
     <DesignAssociationSection />
+    <GetStartedSection
+      title="Ready to Enroll?"
+      text="You can get started with QC Design School whenever you like! When you enroll, you'll receive a login to the Online Student Center where you can access your course online."
+    />
   </>
 );
 
