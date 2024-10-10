92f2630642d2cd65a78a4c9e5f769e10ee3ec1e0
Dave Welsh <dave@qccareerschool.com>
2024-10-10 14:21:20 -0400

    Changed footer links

--- src/app/(main)/footerColumn2.tsx
+++ src/app/(main)/footerColumn2.tsx
@@ -3,11 +3,11 @@ import type { FC } from 'react';
 
 export const FooterColumn2: FC = () => (
   <>
+    <li><Link href="/free-course-catalog">Free Catalog</Link></li>
     <li><Link href="/faq">FAQ</Link></li>
-    <li><Link href="/student-success">Student Success</Link></li>
-    <li><Link href="/instructors">Instructors</Link></li>
-    <li><Link href="/about">About the School</Link></li>
     <li><Link href="/design-associations">Design Associations</Link></li>
+    <li><Link href="/your-career">Your Career</Link></li>
+    <li><Link href="/how-it-works">How It Works</Link></li>
     <li><a href="/blog">Blog</a></li>
     <li><a href="https://studentcenter.qccareerschool.com/students" target="_blank" rel="noreferrer">Student Login</a></li>
   </>
