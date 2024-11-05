4837f3555e8fe303df7943db29f0029d1baba0a3
Owen Moogk <owenmoogk@gmail.com>
2024-11-05 20:15:27 +0100

    Complete career catalyst toolkit page

--- src/app/(main)/career-catalyst-toolkit/page.tsx
+++ src/app/(main)/career-catalyst-toolkit/page.tsx
@@ -7,9 +7,13 @@ import DesignFilesImage from './designfiles.jpg';
 import HeroImage from './hero.jpg';
 import styles from './page.module.scss';
 import videoCallImage from './video-call.jpg';
+import { StatsSection } from '../statsSection';
+import { perks } from './perks';
 import type { PageComponent } from '@/app/serverComponent';
 import { AlertBar } from '@/components/alertBar';
 import { BackgroundImage } from '@/components/backgroundImage';
+import { GetStartedSection } from '@/components/getStartedSection';
+import { GoogleReviewSection } from '@/components/googleReviewSection';
 import XIcon from '@/components/icons/briefcase.svg';
 import { Subtitle } from '@/components/subtitle';
 
@@ -41,9 +45,9 @@ const CareerCatalystToolkitPage: PageComponent = () => (
       <div className="container">
         <h2 className="mb-5 text-center text-lg-start">What's Included in the Career Essentials Kit?</h2>
 
-        <div className="row justify-content-center g-4 g-lg-5">
+        <div className="row justify-content-center g-4 g-lg-5 align-items-center">
           <div className="col-6">
-            <Image src={videoCallImage} alt="" className="img-fluid" />
+            <Image src={videoCallImage} alt="" className={`img-fluid ${styles.roundedImage}`} />
           </div>
           <div className="col-12 col-lg-6">
             <h3 className="h5">Personalized Video Call with an Industry Expert</h3>
@@ -58,9 +62,9 @@ const CareerCatalystToolkitPage: PageComponent = () => (
 
         <hr className="my-5" />
 
-        <div className="row justify-content-center g-4 g-lg-5">
+        <div className="row justify-content-center g-4 g-lg-5 align-items-center">
           <div className="col-6 order-lg-2">
-            <Image src={DesignFilesImage} alt="" className="img-fluid" />
+            <Image src={DesignFilesImage} alt="" className={`img-fluid ${styles.roundedImage}`} />
           </div>
           <div className="col-12 col-lg-6 order-lg-1">
             <h3 className="h5">Four Months of Complimentary Access to DesignFiles</h3>
@@ -75,9 +79,10 @@ const CareerCatalystToolkitPage: PageComponent = () => (
 
         <hr className="my-5" />
 
-        <div className="row justify-content-center g-4 g-lg-5">
+        <div className="row justify-content-center g-4 g-lg-5 align-items-center">
           <div className="col-6">
-            <Image src={CanvaImage} alt="" className="img-fluid" />          </div>
+            <Image src={CanvaImage} alt="" className={`img-fluid ${styles.roundedImage}`} />
+          </div>
           <div className="col-12 col-lg-6">
             <h3 className="h5">Exclusive Canva Template Collection</h3>
             <p>Enhance various aspects of your business with our customized Canva templates.</p>
@@ -90,6 +95,42 @@ const CareerCatalystToolkitPage: PageComponent = () => (
         </div>
       </div>
     </section>
+    <section className="bg-light">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-8 col-xl-7 text-center">
+            <h2 className="mb-4 h4">Take Your Career to the Next Level</h2>
+            <Link href="https://enroll.qcdesignschool.com" className={`btn btn-lg btn-primary ${styles.button}`}>Enroll Now</Link>
+          </div>
+        </div>
+      </div>
+    </section>
+    <StatsSection />
+    <GoogleReviewSection className="bg-light" />
+    <section>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-9 col-xl-8 col-xxl-7 text-lg-center">
+            <h2 className={`mb-4`}>Elevate Your Design Career with the Career Catalyst Toolkit</h2>
+            <p className="lead mb-5">Your dedication to design deserves a strategic and impactful approach. With the Career Catalyst Toolkit, you gain access to expert mentorship, premium resources, and comprehensive support designed to turn your aspirations into reality.</p>
+          </div>
+        </div>
+        <div className="row justify-content-center g-5">
+          {perks.map((perk, index) => (
+            <div className="col-12 col-sm-6 col-lg-4 text-center" key={index}>
+              {perk.icon}
+              <h4 className="my-4">{perk.title}</h4>
+              <p>{perk.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+    <GetStartedSection
+      title="Take the Next Step in Your Design Career"
+      text="Don’t miss out on this exclusive Career Catalyst Toolkit offer. Elevate your design career with the tools, expertise, and support you need to thrive in the competitive design industry. Secure your spot now and embark on a journey of unparalleled professional growth."
+      buttonText="Enroll Now"
+    />
   </>
 );
 
