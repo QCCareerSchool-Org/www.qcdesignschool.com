dc45dc2ae9b8caf6076007fe93ddb47456a17f0b
Dave Welsh <dave@qccareerschool.com>
2024-11-05 18:17:03 -0500

    Reduced width of top section on small screens

--- src/app/(main)/career-catalyst-toolkit/page.tsx
+++ src/app/(main)/career-catalyst-toolkit/page.tsx
@@ -33,7 +33,7 @@ const CareerCatalystToolkitPage: PageComponent = () => (
       <BackgroundImage src={HeroImage} mobile={{ src: HeroImageMobile, breakpoint: 'lg' }} />
       <div className="container">
         <div className="row">
-          <div className="col-12 col-lg-6">
+          <div className="col-10 col-lg-6">
             <div className="mb-3"><Subtitle>Exclusive Opportunity</Subtitle></div>
             <h1 className="mb-3">Career Catalyst Toolkit</h1>
             <p className="mb-5">Take your design career to new heights with our exclusive Career Catalyst Toolkit package. This limited-time offer combines personalized expert guidance, premium <i>Canva</i> templates, and four months of complimentary access to top-tier design tools. Available now to a select number of students, the Career Catalyst Toolkit is your gateway to streamlined workflows, enhanced branding, and a competitive edge in the design industry.</p>
