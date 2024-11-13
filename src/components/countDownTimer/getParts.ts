a55c71ed4182646d64872b95290e213cc26cc689
Owen Moogk <owenmoogk@gmail.com>
2024-11-13 20:05:38 +0100

    Add banner

--- src/components/countDownTimer/getParts.ts
+++ src/components/countDownTimer/getParts.ts
@@ -1,4 +1,8 @@
-export const getParts = (duration: number): [ days: number, hours: number, minutes: number, seconds: number ] => {
-
-  return [ 0, 4, 6, 3 ];
+export const getParts = (durationInMs: number): [ days: number, hours: number, minutes: number, seconds: number ] => {
+  const duration = durationInMs / 1000;
+  const seconds = Math.floor(duration) % 60;
+  const minutes = Math.floor(duration / 60) % 60;
+  const hours = Math.floor(duration / 3600) % 24;
+  const days = Math.floor(duration / 86400);
+  return [ days, hours, minutes, seconds ];
 };
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,14 +2,11 @@ import type { FC, PropsWithChildren } from 'react';
 
 import { Footer } from './footer';
 import { Header } from './header';
-import { CountDownTimer } from '../countDownTimer';
 import { LiveChat } from '@/scripts/liveChat';
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
-  const date = new Date().getTime();
   return (
     <>
-      <CountDownTimer date={date} />
       <Header />
       <main className="flex-shrink-0">{children}</main>
       <Footer />
