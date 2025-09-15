4f58d10e080feccf1af31b72f748f9e2a91336b6
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-09-15 13:41:08 -0400

    Update FS sales page

---
+++ src/app/(landing)/feng-shui-overview/tutorSection.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+
+import { SingleTutorSection } from '@/components/singleTutorSection';
+import TutorImage from '@/components/tutors/ivyEllerbyCollage-transparent.png';
+
+type Props = {
+  className?: string;
+};
+
+export const TutorSection: FC<Props> = ({ className }) => (
+  <SingleTutorSection
+    courseName="Feng Shui"
+    description="When you enroll in QC’s Feng Shui Design course, you’ll learn from Advanced Feng Shui Design Professional, Ivy Ellerby, who will provide personalized feedback on every assignment you submit. Ivy is a consultant with Eastern Design and Home Staging and a published author whose book achieved #1 bestseller status on Amazon. Her deep passion for Feng Shui is evident in every aspect of her work. In her feedback, Ivy combines wisdom and practical insight with an engaging enthusiasm that makes the learning experience both enjoyable and inspiring."
+    src={TutorImage}
+    tutorName="Ivy Ellerby"
+    className={className}
+  />
+);
