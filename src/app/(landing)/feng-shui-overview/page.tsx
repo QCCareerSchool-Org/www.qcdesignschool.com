cbd7d6b0d6135a5dff5bf85102513daa82f09b9e
Dave Welsh <dave@qccareerschool.com>
2025-09-22 13:43:29 -0400

    Fixed missing button text

--- src/app/(landing)/feng-shui-overview/page.tsx
+++ src/app/(landing)/feng-shui-overview/page.tsx
@@ -36,7 +36,7 @@ const enrollUrl = 'https://enroll.qcdesignschool.com/feng-shui-overview?c=fs';
 
 const FengShuiDesignPage: PageComponent = () => (
   <div className={styles.page}>
-    <Header logoLink buttonContent={<><span className="text-light" /><span className="d-none d-sm-inline">Enroll Now</span></>} showBanner buttonHref={enrollUrl} buttonClass={`btn btn-primary btn-md`} buttonAlwaysVisible={true} />
+    <Header logoLink buttonContent="Enroll Now" showBanner buttonHref={enrollUrl} buttonClass={`btn btn-primary btn-md`} buttonAlwaysVisible={true} />
     <section className="half-padding-top bg-light">
       <div className="container">
         <div className="row justify-content-center g-s">
