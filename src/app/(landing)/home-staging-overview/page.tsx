9fe23a09d9b0b54e73ec662be19a79fdb0b3afc3
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-10-17 13:29:58 -0400

    Update red badge for ST overview

--- src/app/(landing)/home-staging-overview/page.tsx
+++ src/app/(landing)/home-staging-overview/page.tsx
@@ -7,7 +7,6 @@ import { CertificationSection } from './certificationSection';
 import { CourseCardsSection } from './courseCardsSection';
 import HeroImage from './hero.jpg';
 import styles from './page.module.scss';
-import RedBadge from './price-badge-$100.png';
 import ShieldIcon from './shield.svg';
 import { TutorSection } from './tutorSection';
 import WhatYoullLearnImage from './what-youll-learn.jpg';
@@ -24,6 +23,7 @@ import { PaymentPlanSection } from '@/components/paymentPlanSection';
 import { TestimonialWallSection } from '@/components/testimonialWallSection';
 import { VirtualCommunitySection } from '@/components/virtualCommunitySection';
 import type { CourseCode } from '@/domain/courseCode';
+import { gbpCountry } from '@/lib/currencies';
 import { getData } from '@/lib/getData';
 
 export const metadata: Metadata = {
@@ -37,6 +37,7 @@ const courseCodes: CourseCode[] = [ 'st' ];
 
 const HomeStagingPage: PageComponent = async () => {
   const { countryCode } = await getData();
+  const redBadgePath = gbpCountry(countryCode) ? '/images/price-badge-$100-uk.png' : '/images/price-badge-$100.png';
 
   return (
     <div className={styles.page}>
@@ -50,11 +51,13 @@ const HomeStagingPage: PageComponent = async () => {
                 <h1 className="mb-2 mb-xl-4">Home Staging Course</h1>
                 <p className="h5 mb-2">Become a Certified Home Stager</p>
                 <p className="">Enroll Today and Start Booking Clients in Under 3 Months!</p>
-                <div className="d-block d-md-none text-center mb-4">
-                  <Image src={RedBadge} alt="" style={{ width: '35%', height: 'auto' }} />
+                <div className="d-block d-md-none text-center">
+                  <Image src={redBadgePath} alt="" width={150} height={150} />
                 </div>
               </Hero>
-              <Image src={RedBadge} alt="" className="position-absolute d-none d-md-block" style={{ bottom: '25.5rem', right: '3.5rem', width: '17%', height: 'auto' }} />
+              <div className="position-absolute d-none d-md-block" style={{ bottom: '26rem', right: '3.5rem', position: 'relative', width: '17%', paddingBottom: '15%', height: 0 }}>
+                <Image src={redBadgePath} alt="" fill={true} style={{ objectFit: 'contain' }} />
+              </div>
             </div>
             <div className="col-12 col-md-10 col-lg-8 text-center">
               <p className="mb-4">This is your opportunity to earn a professional certification and launch the home staging career you&rsquo;ve been dreaming of, creating beautiful, market-ready homes. Join over 45,000 QC students and graduates worldwide who have turned their passion for design into successful, thriving businesses. Don&rsquo;t wait&mdash;take the first step toward your future in home staging today!</p>
