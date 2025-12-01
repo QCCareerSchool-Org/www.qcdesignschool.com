4205c0c2a72b7909625c594509085cf5e4db2d71
Dave Welsh <dave@qccareerschool.com>
2025-12-01 11:49:59 -0500

    Fixed typo

--- src/components/promotions/cyberMonday2025/index.tsx
+++ src/components/promotions/cyberMonday2025/index.tsx
@@ -17,7 +17,7 @@ export const CyberMonday2025: FC<Props> = ({ countryCode }) => {
       <div className="container text-center">
         <div className="row justify-content-center mt-2">
           <div className="col-12 col-md-10 col-lg-8 col-xl-7">
-            <h2 className="mb-3">Cybmer Monday Savings Are Here</h2>
+            <h2 className="mb-3">Cyber Monday Savings Are Here</h2>
             <div className="mb-4">
               <a href="https://enroll.qcdesignschool.com"><Image src={heroImageSrc} alt="" className="img-fluid" /></a>
             </div>
