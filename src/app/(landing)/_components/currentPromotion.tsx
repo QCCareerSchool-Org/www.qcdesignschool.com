ff77fbdd1a51352869d68b0002f4212f50de06ac
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-19 14:11:44 -0500

    Add 100-off promo section

--- src/app/(landing)/_components/currentPromotion.tsx
+++ src/app/(landing)/_components/currentPromotion.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 
+import { Jan2025_100Off } from '@/components/promotions/jan2025-100off';
 import { NewYears2025 } from '@/components/promotions/newYears2025';
 
 interface Props {
@@ -10,5 +11,9 @@ interface Props {
 export const CurrentPromotion: FC<Props> = ({ date, countryCode }) => {
   if (date >= Date.UTC(2025, 11, 26, 8) && date < Date.UTC(2026, 0, 3, 8)) { // 2025-12-26T03:00 (8:00 UTC) to 2026-01-03T03:00 (8:00 UTC)
     return <NewYears2025 />;
+  } else if (date >= Date.UTC(2026, 0, 7, 8) && date < Date.UTC(2026, 0, 17, 8)) { // 2026-01-07T03:00 (8:00 UTC) to 2026-01-17T03:00 (8:00 UTC)
+    return <Jan2025_100Off countryCode={countryCode} />;
   }
+
+  return null;
 };
---
+++ src/components/promotions/jan2025-100off/index.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import type { FC } from 'react';
+
+import HeroImageUK from './desktop-uk.jpg';
+import HeroImageDefault from './desktop.jpg';
+import { gbpCountry } from '@/lib/currencies';
+
+interface Props {
+  countryCode: string;
+}
+
+export const Jan2025_100Off: FC<Props> = ({ countryCode }) => {
+  const heroImageSrc = gbpCountry(countryCode) ? HeroImageUK : HeroImageDefault;
+
+  return (
+    <section className='bg-light'>
+      <div className="container text-center">
+        <div className="row justify-content-center mt-2">
+          <div className="col-12 col-md-10 col-lg-8 col-xl-7">
+            <h2>Limited Time Offer</h2>
+            <p className="mb-4">Start the new year by investing in your future. For a limited time, enroll in any design course and <strong>get a second course FREE, plus {gbpCountry(countryCode) ? '£100' : '$100'} OFF your tuition</strong>&mdash;so you can double your expertise! Start today and become a certified design expert in under 3 months.</p>
+            <div className="mb-4">
+              <a href="https://enroll.qcdesignschool.com"><Image src={heroImageSrc} alt="" className="img-fluid" /></a>
+            </div>
+            <a href="https://enroll.qcdesignschool.com"><button className="btn btn-primary btn-lg">Claim Your Free Course</button></a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
