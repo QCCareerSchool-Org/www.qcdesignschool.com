bdc41bbf66cf59a825ba7fd30ff9478b32ad5cb4
Dave Welsh <dave@qccareerschool.com>
2025-12-22 11:47:18 -0500

    Removed variable

--- src/app/(landing)/_components/currentPromotion.tsx
+++ src/app/(landing)/_components/currentPromotion.tsx
@@ -7,7 +7,7 @@ interface Props {
   countryCode: string;
 }
 
-export const CurrentPromotion: FC<Props> = ({ date, countryCode }) => {
+export const CurrentPromotion: FC<Props> = ({ date }) => {
   if (date >= Date.UTC(2025, 11, 26, 8) && date < Date.UTC(2026, 0, 3, 8)) { // 2025-12-26T03:00 (8:00 UTC) to 2026-01-03T03:00 (8:00 UTC)
     return <NewYears2025 />;
   }
