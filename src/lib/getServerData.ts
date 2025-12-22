23722854a87be838e27d15b231e1e563b0bae3c5
Dave Welsh <dave@qccareerschool.com>
2025-12-22 15:56:04 -0500

    Updated return type

--- src/lib/getServerData.ts
+++ src/lib/getServerData.ts
@@ -4,13 +4,24 @@ import { headers } from 'next/headers';
 
 import { getParam } from './getParam';
 
-interface Data {
+interface BaseData {
   countryCode: string;
   provinceCode: string | null;
+}
+
+interface DataWithDate extends BaseData {
   date: number;
 }
 
-export const getServerData = async (searchParams?: Promise<Record<string, string | string[] | undefined>>): Promise<Data> => {
+export function getServerData(): Promise<BaseData>;
+
+export function getServerData(
+  searchParams: Promise<Record<string, string | string[] | undefined>>,
+): Promise<DataWithDate>;
+
+export async function getServerData(
+  searchParams?: Promise<Record<string, string | string[] | undefined>>,
+): Promise<BaseData | DataWithDate> {
   const headerList = await headers();
   const countryCode = headerList.get('x-vercel-ip-country') ?? 'US';
   const provinceCode = headerList.get('x-vercel-ip-country-region');
