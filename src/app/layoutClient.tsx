a2c76d69bc15919d572d667b0fb4e9c9b254dcb2
Dave Welsh <dave@qccareerschool.com>
2026-01-19 13:57:21 -0500

    Fix fbq page views

--- src/app/layoutClient.tsx
+++ src/app/layoutClient.tsx
@@ -5,6 +5,7 @@ import type { FC } from 'react';
 import { useEffect, useRef } from 'react';
 
 import { brevoPageview } from '@/lib/brevo';
+import { fbqPageview } from '@/lib/fbq';
 
 export const LayoutClient: FC = () => {
   const countRef = useRef(0);
@@ -12,16 +13,19 @@ export const LayoutClient: FC = () => {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    if (countRef.current > 0) { // don't run the first time because it's already tracked
+    let url = `https://www.qcdesignschool.com${pathname}`;
+    const stringSearchParams = searchParams.toString();
+    if (stringSearchParams) {
+      url += `?${stringSearchParams}`;
+    }
+
+    if (countRef.current > 0) { // don't run the first time because it's already being tracked in the snippet
       if (pathname) {
-        let url = `https://www.qcdesignschool.com${pathname}`;
-        const stringSearchParams = searchParams.toString();
-        if (stringSearchParams) {
-          url += `?${stringSearchParams}`;
-        }
         const title = document.title;
         brevoPageview(title, url, pathname);
       }
+    } else { // only do this the first time because we exclude the initial pageView in the snippet, but autoconfig handles all other views
+      fbqPageview(url);
     }
     countRef.current++;
   }, [ pathname, searchParams ]);
