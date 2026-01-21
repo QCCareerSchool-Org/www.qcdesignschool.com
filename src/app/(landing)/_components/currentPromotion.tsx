a6bddab563ce52b5d71b59d06b7e8860cf281fa5
Dave Welsh <dave@qccareerschool.com>
2026-01-21 10:13:49 -0500

    Added banners and thank-you page promo images

--- src/app/(landing)/_components/currentPromotion.tsx
+++ src/app/(landing)/_components/currentPromotion.tsx
@@ -2,9 +2,8 @@
 
 import type { FC } from 'react';
 
-import { EndOfYear2025 } from '@/components/promotions/endOfYear2025';
 import { NewYear2026 } from '@/components/promotions/NewYear2026';
-import { endOfYear2025, newYear2026 } from '@/lib/promotionPeriods';
+import { jan21Period } from '@/lib/promotionPeriods';
 
 interface Props {
   date: number;
@@ -12,9 +11,7 @@ interface Props {
 }
 
 export const CurrentPromotion: FC<Props> = ({ date, countryCode }) => {
-  if (endOfYear2025.contains(date)) {
-    return <EndOfYear2025 />;
-  } else if (newYear2026.contains(date)) {
+  if (jan21Period.contains(date)) {
     return <NewYear2026 countryCode={countryCode} />;
   }
 };
