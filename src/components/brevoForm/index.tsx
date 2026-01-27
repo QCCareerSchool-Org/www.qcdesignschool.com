aebefb40d9aa383fe417554b61f9c1e607b409d1
Dave Welsh <dave@qccareerschool.com>
2026-01-27 16:58:15 -0500

    Fixed field name

--- src/components/brevoForm/index.tsx
+++ src/components/brevoForm/index.tsx
@@ -106,7 +106,7 @@ export const BrevoForm: FC<Props> = props => {
     <form action="https://leads.qccareerschool.com" method="post" className={styles.brochureForm} onSubmit={handleSubmit}>
       <CurrentPageInput />
       <JavasciptInput />
-      <input type="hidden" name="return" value="0" />
+      <input type="hidden" name="forward" value="0" />
       <input type="hidden" name="nonce" value={nonce} />
       <input type="hidden" name="g-recaptcha-response" value={token} />
       <input type="hidden" name="school" value="QC Design School" />
