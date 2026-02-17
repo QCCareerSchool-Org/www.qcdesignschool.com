955127cc83d6780ac4ab4a218c1fb30e7383424f
Dave Welsh <dave@qccareerschool.com>
2026-02-17 12:20:22 -0500

    Updated banner

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -6,7 +6,7 @@ import { MainNav } from './mainNav';
 import { PromoBanner } from './promoBanner';
 import { gbpCountry } from '@/lib/currencies';
 import { getDesignRestricted } from '@/lib/restrictions';
-import { feb04, feb13 } from '@/periods';
+import { feb19 } from '@/periods';
 
 interface Props {
   date: number;
@@ -21,19 +21,13 @@ export const Header: FC<Props> = ({ date, countryCode, provinceCode }) => {
   return (
     <header className={`${styles.header} flex-shrink-0`} style={{ position: 'sticky', top: 0, zIndex: 1050, width: '100%' }}>
       {/* {countryCode === 'CA' && <CanadaHeader />} */}
-      {feb04.contains(date)
+      {feb19.contains(date)
         ? (
-          <PromoBanner date={date} promotionPeriod={feb04.toDTO()}>
-            <span className="d-none d-lg-inline">Don't Miss Out—</span>Enroll Today and Get a Second Course <strong>Free!</strong>
+          <PromoBanner date={date} promotionPeriod={feb19.toDTO()}>
+            <span className="d-none d-lg-inline">Limited-Time Offer—</span>Get a Second Course <strong>Free</strong> + {discount} off
           </PromoBanner>
         )
-        : feb13.contains(date)
-          ? (
-            <PromoBanner date={date} promotionPeriod={feb13.toDTO()}>
-              <span className="d-none d-lg-inline">Limited-Time Offer—</span>Get a Second Course <strong>Free</strong> + {discount} off
-            </PromoBanner>
-          )
-          : null
+        : null
       }
       <MainNav designRestricted={designRestricted} />
     </header>
