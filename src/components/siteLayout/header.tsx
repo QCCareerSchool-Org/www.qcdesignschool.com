674e1ba432b33ffd6a2f3aa8a2e25ef57de55b8a
Dave Welsh <dave@qccareerschool.com>
2026-05-20 10:35:28 -0400

    Added Canada tax header override

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -2,8 +2,8 @@ import type { FC } from 'react';
 
 import styles from './header.module.css';
 import { MainNav } from './mainNav';
-// import { CanadaHeader } from '@/components/canadaHeader';
 import { PromoBanner } from './promoBanner';
+import { CanadaHeader } from '../canadaHeader';
 import { getDesignRestricted } from '@/lib/restrictions';
 import { may16 } from '@/periods';
 
@@ -13,24 +13,30 @@ interface Props {
   provinceCode: string | null;
 }
 
-export const Header: FC<Props> = ({ date, countryCode, provinceCode }) => {
-  const designRestricted = getDesignRestricted(countryCode, provinceCode);
+export const Header: FC<Props> = props => {
+  const designRestricted = getDesignRestricted(props.countryCode, props.provinceCode);
 
   return (
     <header className={`${styles.header} flex-shrink-0`} style={{ position: 'sticky', top: 0, zIndex: 1050, width: '100%' }}>
-      {/* {countryCode === 'CA' && <CanadaHeader />} */}
-      {may16.contains(date)
-        ? (
-          <PromoBanner date={date} promotionPeriod={may16.toDTO()}>
-            {countryCode === 'US'
-              ? <>Memorial Week Special: Get a Free Second Course</>
-              : <><span className="d-none d-lg-inline">Ends Soon&mdash;</span><strong>Get a Free Second Course</strong></>
-            }
-          </PromoBanner>
-        )
-        : null
-      }
+      <InnerBanner {...props} />
       <MainNav designRestricted={designRestricted} />
     </header>
   );
+};
+
+const InnerBanner: FC<Props> = ({ date, countryCode }) => {
+  if (countryCode === 'CA') {
+    return <CanadaHeader />;
+  }
+
+  if (may16.contains(date)) {
+    return (
+      <PromoBanner date={date} promotionPeriod={may16.toDTO()}>
+        {countryCode === 'US'
+          ? <>Memorial Week Special: Get a Free Second Course</>
+          : <><span className="d-none d-lg-inline">Ends Soon&mdash;</span><strong>Get a Free Second Course</strong></>
+        }
+      </PromoBanner>
+    );
+  }
 };
