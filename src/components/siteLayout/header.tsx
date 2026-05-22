6b65ccd80ff46bcc9766fc84dd885e19edaccb2f
Angela Li <angelayxli@gmail.com>
2026-05-22 17:08:23 -0400

    simplified nav logic

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -4,6 +4,7 @@ import styles from './header.module.css';
 import { MainNav } from './mainNav';
 import { PromoBanner } from './promoBanner';
 import { CanadaHeader } from '../canadaHeader';
+import { getServerData } from '@/lib/getServerData';
 import { getDesignRestricted } from '@/lib/restrictions';
 import { may16 } from '@/periods';
 
@@ -13,13 +14,14 @@ interface Props {
   provinceCode: string | null;
 }
 
-export const Header: FC<Props> = props => {
+export const Header: FC<Props> = async props => {
   const designRestricted = getDesignRestricted(props.countryCode, props.provinceCode);
+  const { countryCode } = await getServerData();
 
   return (
     <header className={`${styles.header} flex-shrink-0`} style={{ position: 'sticky', top: 0, zIndex: 1050, width: '100%' }}>
       <InnerBanner {...props} />
-      <MainNav designRestricted={designRestricted} />
+      <MainNav designRestricted={designRestricted} countryCode={countryCode} />
     </header>
   );
 };
