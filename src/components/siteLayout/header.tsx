99f77b6d2b51db5637a304b4b315a187596eab42
Dave Welsh <dave@qccareerschool.com>
2026-05-25 16:42:17 -0400

    Use existing prop

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -4,7 +4,6 @@ import styles from './header.module.css';
 import { MainNav } from './mainNav';
 import { PromoBanner } from './promoBanner';
 import { CanadaHeader } from '../canadaHeader';
-import { getServerData } from '@/lib/getServerData';
 import { getDesignRestricted } from '@/lib/restrictions';
 import { may16 } from '@/periods';
 
@@ -14,14 +13,13 @@ interface Props {
   provinceCode: string | null;
 }
 
-export const Header: FC<Props> = async props => {
+export const Header: FC<Props> = props => {
   const designRestricted = getDesignRestricted(props.countryCode, props.provinceCode);
-  const { countryCode } = await getServerData();
 
   return (
     <header className={`${styles.header} flex-shrink-0`} style={{ position: 'sticky', top: 0, zIndex: 1050, width: '100%' }}>
       <InnerBanner {...props} />
-      <MainNav designRestricted={designRestricted} countryCode={countryCode} />
+      <MainNav designRestricted={designRestricted} countryCode={props.countryCode} />
     </header>
   );
 };
