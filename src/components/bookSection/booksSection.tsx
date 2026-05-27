ee4979311b0552f2db95905d56bc8ea34643ea1b
Dave Welsh <dave@qccareerschool.com>
2026-05-27 15:45:31 -0400

    Updated mobile layout

--- src/components/bookSection/booksSection.tsx
+++ src/components/bookSection/booksSection.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import type { FC } from 'react';
 import { BiBookHeart } from 'react-icons/bi';
 
@@ -6,17 +7,20 @@ import HeroImageBookMobile from './background-mobile.jpg';
 import { BackgroundImage } from '../backgroundImage';
 
 export const BookSection: FC = () => (
-  <section>
-    <BackgroundImage priority src={HeroImageBookDesktop} objectPosition="37.5% 25%" mobile={{ src: HeroImageBookMobile, breakpoint: 'lg', objectPosition: '50% 87.5%' }} />
-    <div className="row text-white position-relative z-1 p-5">
-      <div className="col-6" />
-      <div className="col-3">
-        <h2 className="text-white">Course</h2>
-        <h2 className="text-white">Textbooks Included</h2>
-        <h5 className="mb-3 fw-light text-white"><i>with your enrollment</i></h5>
-        <p>Enroll in the Interior Design Course Accelerator today and receive QC's exclusive physical textbooks as a complimentary bonus.</p>
-        <p><i>Take your studying offline with premium course materials shipped straight to your door. </i></p>
-        <a className="btn text-white" href="https://enroll.qcdesignschool.com/" style={{ backgroundColor: '#c19e76' }}><BiBookHeart /> Enroll Now & Claim Your Books</a>
+  <section className="text-white" style={{ backgroundColor: '#191514' }}>
+    <div className="d-none d-lg-block"><BackgroundImage priority src={HeroImageBookDesktop} objectPosition="37.5% 25%" /></div>
+    <div className="container">
+      <div className="row g-5 g-lg-s justify-content-center justify-content-lg-end">
+        <div className="col-12 col-sm-10 col-md-8 col-lg-6 d-lg-none">
+          <Image src={HeroImageBookMobile} className="img-fluid rounded-3" alt="" />
+        </div>
+        <div className="col-12 col-lg-6">
+          <h2 className="text-white">Course Textbooks Included</h2>
+          <p className="lead mb-4"><i>with your enrollment</i></p>
+          <p>Enroll in the Interior Design Course Accelerator today and receive QC's exclusive physical textbooks as a complimentary bonus.</p>
+          <p><i>Take your studying offline with premium course materials shipped straight to your door.</i></p>
+          <a className="btn btn-lg text-white" href="https://enroll.qcdesignschool.com" style={{ backgroundColor: '#c19e76' }}><BiBookHeart size={20} style={{ position: 'relative', top: -1 }} /> Enroll Now & Claim Your Books</a>
+        </div>
       </div>
     </div>
   </section>
