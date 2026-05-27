6aa155fb17014e596a06b34556856cda5ade03e2
Dave Welsh <dave@qccareerschool.com>
2026-05-27 15:10:46 -0400

    Updated prices endpoint

--- src/lib/restrictions.ts
+++ src/lib/restrictions.ts
@@ -1,6 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getDesignRestricted = (countryCode: string, provinceCode: string | null): boolean => {
-  return countryCode === 'CA' ||
-    (countryCode === 'US' && provinceCode === 'LA') ||
-    (countryCode === 'US' && provinceCode === 'NV') ||
-    (countryCode === 'US' && provinceCode === 'DC');
+  return true;
+  // return countryCode === 'CA' ||
+  //   (countryCode === 'US' && provinceCode === 'LA') ||
+  //   (countryCode === 'US' && provinceCode === 'NV') ||
+  //   (countryCode === 'US' && provinceCode === 'DC');
 };
