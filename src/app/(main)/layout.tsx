d87ab3a66b3a7ed5d4ee496ca20595cac076e321
Dave Welsh <dave@qccareerschool.com>
2026-05-28 11:13:21 -0400

    deploy

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,8 @@
 import { SiteLayout } from '@/components/siteLayout';
 import type { LayoutComponent } from '@/serverComponent';
 
+import '../bootstrap.scss';
+
 const MainLayout: LayoutComponent = ({ children }) => <SiteLayout>{children}</SiteLayout>;
 
 export default MainLayout;
